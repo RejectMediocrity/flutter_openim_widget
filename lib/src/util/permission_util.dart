24cfeb6addccac51ec36c6a7e51451b139843cb1
Logan <liubin3310@gmail.com>
2022-04-11 11:20:53 +0800

    权限申请失败回调

--- lib/src/util/permission_util.dart
+++ lib/src/util/permission_util.dart
@@ -4,75 +4,123 @@ import 'package:permission_handler/permission_handler.dart';
 class PermissionUtil {
   PermissionUtil._();
 
-  static void camera(Function() onGranted) async {
-    if (await p.Permission.camera.request().isGranted) {
-      // Either the permission was already granted before or the user just granted it.
-      onGranted();
-    }
-    if (await p.Permission.camera.isPermanentlyDenied) {
-      // The user opted to never again see the permission request dialog for this
-      // app. The only way to change the permission's status now is to let the
-      // user manually enable it in the system settings.
-    }
-  }
-
-  static void storage(Function() onGranted) async {
-    if (await p.Permission.storage.request().isGranted) {
-      // Either the permission was already granted before or the user just granted it.
-      onGranted();
-    }
-    if (await p.Permission.storage.isPermanentlyDenied) {
-      // The user opted to never again see the permission request dialog for this
-      // app. The only way to change the permission's status now is to let the
-      // user manually enable it in the system settings.
+  static void camera(Function() onGranted,
+      {Function(PermissionStatus)? onFailed}) async {
+    try {
+      PermissionStatus status = await p.Permission.camera.request();
+      if (status.isGranted) {
+        // Either the permission was already granted before or the user just granted it.
+        onGranted();
+      } else {
+        if (onFailed != null) onFailed(status);
+      }
+      if (await p.Permission.camera.isPermanentlyDenied) {
+        // The user opted to never again see the permission request dialog for this
+        // app. The only way to change the permission's status now is to let the
+        // user manually enable it in the system settings.
+      }
+    } catch (e) {
+      if (onFailed != null) onFailed(p.PermissionStatus.permanentlyDenied);
     }
   }
 
-  static void microphone(Function() onGranted) async {
-    if (await p.Permission.microphone.request().isGranted) {
-      // Either the permission was already granted before or the user just granted it.
-      onGranted();
-    }
-    if (await p.Permission.microphone.isPermanentlyDenied) {
-      // The user opted to never again see the permission request dialog for this
-      // app. The only way to change the permission's status now is to let the
-      // user manually enable it in the system settings.
-    }
-  }
-
-  static void location(Function() onGranted) async {
-    if (await p.Permission.location.request().isGranted) {
-      // Either the permission was already granted before or the user just granted it.
-      onGranted();
-    }
-    if (await p.Permission.location.isPermanentlyDenied) {
-      // The user opted to never again see the permission request dialog for this
-      // app. The only way to change the permission's status now is to let the
-      // user manually enable it in the system settings.
+  static void storage(Function() onGranted,
+      {Function(PermissionStatus)? onFailed}) async {
+    try {
+      PermissionStatus status = await p.Permission.storage.request();
+      if (status.isGranted) {
+        // Either the permission was already granted before or the user just granted it.
+        onGranted();
+      } else {
+        if (onFailed != null) onFailed(status);
+      }
+      if (await p.Permission.storage.isPermanentlyDenied) {
+        // The user opted to never again see the permission request dialog for this
+        // app. The only way to change the permission's status now is to let the
+        // user manually enable it in the system settings.
+      }
+    } catch (e) {
+      if (onFailed != null) onFailed(p.PermissionStatus.permanentlyDenied);
     }
   }
 
-  static void speech(Function() onGranted) async {
-    if (await p.Permission.speech.request().isGranted) {
-      // Either the permission was already granted before or the user just granted it.
-      onGranted();
-    }
-    if (await p.Permission.speech.isPermanentlyDenied) {
-      // The user opted to never again see the permission request dialog for this
-      // app. The only way to change the permission's status now is to let the
-      // user manually enable it in the system settings.
+  static void microphone(Function() onGranted,
+      {Function(PermissionStatus)? onFailed}) async {
+    try {
+      PermissionStatus status = await p.Permission.microphone.request();
+      if (status.isGranted) {
+        // Either the permission was already granted before or the user just granted it.
+        onGranted();
+      } else {
+        if (onFailed != null) onFailed(status);
+      }
+      if (await p.Permission.microphone.isPermanentlyDenied) {
+        // The user opted to never again see the permission request dialog for this
+        // app. The only way to change the permission's status now is to let the
+        // user manually enable it in the system settings.
+      }
+    } catch (e) {
+      if (onFailed != null) onFailed(p.PermissionStatus.permanentlyDenied);
     }
   }
 
-  static void photos(Function() onGranted) async {
-    if (await p.Permission.photos.request().isGranted) {
-      // Either the permission was already granted before or the user just granted it.
-      onGranted();
+  static void location(Function() onGranted,
+      {Function(PermissionStatus)? onFailed}) async {
+    try {
+      PermissionStatus status = await p.Permission.location.request();
+      if (status.isGranted) {
+        // Either the permission was already granted before or the user just granted it.
+        onGranted();
+      } else {
+        if (onFailed != null) onFailed(status);
+      }
+      if (await p.Permission.location.isPermanentlyDenied) {
+        // The user opted to never again see the permission request dialog for this
+        // app. The only way to change the permission's status now is to let the
+        // user manually enable it in the system settings.
+      }
+    } catch (e) {
+      if (onFailed != null) onFailed(p.PermissionStatus.permanentlyDenied);
     }
-    if (await p.Permission.photos.isPermanentlyDenied) {
-      // The user opted to never again see the permission request dialog for this
-      // app. The only way to change the permission's status now is to let the
-      // user manually enable it in the system settings.
+  }
+
+  static void speech(Function() onGranted,
+      {Function(PermissionStatus)? onFailed}) async {
+    try {
+      PermissionStatus status = await p.Permission.speech.request();
+      if (status.isGranted) {
+        // Either the permission was already granted before or the user just granted it.
+        onGranted();
+      } else {
+        if (onFailed != null) onFailed(status);
+      }
+      if (await p.Permission.speech.isPermanentlyDenied) {
+        // The user opted to never again see the permission request dialog for this
+        // app. The only way to change the permission's status now is to let the
+        // user manually enable it in the system settings.
+      }
+    } catch (e) {
+      if (onFailed != null) onFailed(p.PermissionStatus.permanentlyDenied);
+    }
+  }
+
+  static void photos(Function() onGranted,
+      {Function(PermissionStatus)? onFailed}) async {
+    try {
+      PermissionStatus status = await p.Permission.photos.request();
+      if (status.isGranted) {
+        // Either the permission was already granted before or the user just granted it.
+        onGranted();
+      } else {
+        if (onFailed != null) onFailed(status);
+      }
+      if (await p.Permission.photos.isPermanentlyDenied) {
+        // The user opted to never again see the permission request dialog for this
+        // app. The only way to change the permission's status now is to let the
+        // user manually enable it in the system settings.
+      }
+    } catch (e) {
+      if (onFailed != null) onFailed(p.PermissionStatus.permanentlyDenied);
     }
   }
 
