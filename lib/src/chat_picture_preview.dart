a18fec7221d3fd57b57992bd7fafb57b51b81beb
Logan <liubin3310@gmail.com>
2022-05-06 16:15:02 +0800

    图片预览功能改版

--- lib/src/chat_picture_preview.dart
+++ lib/src/chat_picture_preview.dart
@@ -7,21 +7,30 @@ import 'package:flutter_openim_widget/flutter_openim_widget.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class PicInfo {
+  final String? thumbUrl;
   final String? url;
   final File? file;
-
-  PicInfo({this.url, this.file});
+  final int? size;
+  bool? showSourcePic; // 查看原图
+  PicInfo({
+    this.url,
+    this.file,
+    this.thumbUrl,
+    this.size,
+    this.showSourcePic = false,
+  });
 }
 
 class ChatPicturePreview extends StatefulWidget {
-  ChatPicturePreview({
-    Key? key,
-    required this.picList,
-    this.index = 0,
-    this.tag,
-    this.onDownload,
-    this.onTap,
-  })  : this.controller = ExtendedPageController(
+  ChatPicturePreview(
+      {Key? key,
+      required this.picList,
+      this.index = 0,
+      this.tag,
+      this.onDownload,
+      this.onTap,
+      this.showMenu})
+      : this.controller = ExtendedPageController(
           initialPage: index,
           pageSpacing: 10,
         ),
@@ -32,25 +41,32 @@ class ChatPicturePreview extends StatefulWidget {
   final ExtendedPageController controller;
   final Future<bool> Function(String)? onDownload;
   final Function()? onTap;
+  final Function()? showMenu;
 
   @override
   State<ChatPicturePreview> createState() => _ChatPicturePreviewState();
 }
 
 class _ChatPicturePreviewState extends State<ChatPicturePreview> {
-  double initScale = 1.0;
+  late int currentPage;
+  late List<PicInfo> picList;
+  @override
+  void initState() {
+    currentPage = widget.index;
+    picList = List.from(widget.picList);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     var child = Stack(
       children: [
         _buildPageView(),
-        _buildBackBtn(onTap: () => Navigator.pop(context)),
-        _buildToolsBtn(onDownload: () {
-          int index = widget.controller.page?.toInt() ?? 0;
-          if (index < widget.picList.length) {
-            widget.onDownload?.call(widget.picList.elementAt(index).url!);
-          }
+        _buildToolsView(onDownload: () {
+          downLoadImg();
+        }, onViewOriginImg: () {
+          picList[currentPage].showSourcePic = true;
+          setState(() {});
         }),
       ],
     );
@@ -60,8 +76,19 @@ class _ChatPicturePreviewState extends State<ChatPicturePreview> {
     );
   }
 
+  void downLoadImg() {
+    if (currentPage < picList.length) {
+      widget.onDownload?.call(picList.elementAt(currentPage).url!);
+    }
+  }
+
+  void close() {
+    Navigator.pop(context);
+  }
+
   Widget _buildChildView(int index) {
-    var info = widget.picList.elementAt(index);
+    var info = picList.elementAt(index);
+    String? url = info.showSourcePic == true ? info.url : info.thumbUrl;
     if (info.file != null) {
       return ExtendedImage.file(
         info.file!,
@@ -71,9 +98,9 @@ class _ChatPicturePreviewState extends State<ChatPicturePreview> {
         loadStateChanged: _buildLoadStateChangedView,
         initGestureConfigHandler: _buildGestureConfig,
       );
-    } else if (info.url != null) {
+    } else if (url != null) {
       return ExtendedImage.network(
-        info.url!,
+        url,
         fit: BoxFit.contain,
         mode: ExtendedImageMode.gesture,
         clearMemoryCacheWhenDispose: true,
@@ -89,7 +116,7 @@ class _ChatPicturePreviewState extends State<ChatPicturePreview> {
   GestureConfig _buildGestureConfig(ExtendedImageState state) => GestureConfig(
         //you must set inPageView true if you want to use ExtendedImageGesturePageView
         inPageView: true,
-        initialScale: initScale,
+        initialScale: 1.0,
         maxScale: 10.0,
         animationMaxScale: 12.0,
         initialAlignment: InitialAlignment.center,
@@ -133,70 +160,77 @@ class _ChatPicturePreviewState extends State<ChatPicturePreview> {
   }
 
   Widget _buildPageView() => GestureDetector(
-        onTap: widget.onTap,
-        onDoubleTap: () {
-          setState(() {
-            initScale = 1.5;
-          });
-        },
+        onTap: widget.onTap ?? close,
         child: ExtendedImageGesturePageView.builder(
           controller: widget.controller,
-          itemCount: widget.picList.length,
+          itemCount: picList.length,
           itemBuilder: (BuildContext context, int index) {
             return _buildChildView(index);
+          },
+          onPageChanged: (int index) {
+            currentPage = index;
           },
         ),
       );
 
-  Widget _buildToolsBtn({Function()? onDownload}) => Positioned(
-        top: 676.h,
-        width: 375.w,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GestureDetector(
-              behavior: HitTestBehavior.translucent,
-              onTap: onDownload,
-              child: Container(
-                width: 100.w,
-                height: 30.h,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  // color: Color(0xFFFFFFFF).withOpacity(0.23),
-                  color: Colors.grey.withOpacity(0.5),
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Text(
-                  UILocalizations.download,
-                  style: TextStyle(
-                    fontSize: 14.sp,
-                    color: Colors.white,
-                  ),
-                ),
+  Widget _buildToolsView(
+      {Function()? onViewOriginImg, Function()? onDownload}) {
+    int size = picList[currentPage].size ?? 0;
+    String sizeStr = CommonUtil.formatBytes(size);
+    return Positioned(
+      bottom: MediaQuery.of(context).padding.bottom + 16.w,
+      child: Row(
+        children: [
+          SizedBox(
+            width: 118.w,
+          ),
+          GestureDetector(
+            onTap: onViewOriginImg,
+            child: Container(
+              width: 140.w,
+              height: 32.w,
+              decoration: BoxDecoration(
+                color: Color(0xFF999999).withAlpha(76),
+                borderRadius: BorderRadius.circular(100.w),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                "查看原图($sizeStr)",
+                style: TextStyle(fontSize: 12.sp, color: Colors.white),
               ),
             ),
-          ],
-        ),
-      );
-
-  Widget _buildBackBtn({Function()? onTap}) => Positioned(
-        top: 35.h,
-        left: 30.w,
-        child: GestureDetector(
-          behavior: HitTestBehavior.translucent,
-          onTap: onTap,
-          child: Container(
-            width: 38,
-            height: 38,
-            decoration: BoxDecoration(
-              color: Colors.black87.withOpacity(0.4),
-              shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Icon(Icons.close, color: Colors.white),
           ),
-        ),
-      );
+          SizedBox(
+            width: 20.w,
+          ),
+          GestureDetector(
+            onTap: widget.showMenu,
+            child: ImageUtil.assetImage(
+              "preview_but_thumbnail_background",
+              width: 32.w,
+              height: 32.w,
+              fit: BoxFit.fill,
+            ),
+          ),
+          SizedBox(
+            width: 16.w,
+          ),
+          GestureDetector(
+            onTap: onDownload,
+            child: ImageUtil.assetImage(
+              "preview_but_download_background",
+              width: 32.w,
+              height: 32.w,
+              fit: BoxFit.fill,
+            ),
+          ),
+          SizedBox(
+            width: 16.w,
+          ),
+        ],
+      ),
+    );
+  }
 
   Widget _buildErrorView() => Container(
         width: 375.w,
