423aa68cc104a363b5182c8d70419519a937ba61
Logan <liubin3310@gmail.com>
2022-06-28 09:58:25 +0800

    refactor:解决emoji表情展示异常问题

--- lib/src/util/common_util.dart
+++ lib/src/util/common_util.dart
@@ -150,7 +150,9 @@ class CommonUtil {
 
   /// Text 在中英文混排是会出现截取不完整的问题
   static String breakWord(String text) {
-    if (text.isEmpty) {
+    RegExp regexp=RegExp("[^\\u0020-\\u007E\\u00A0-\\u00BE\\u2E80-\\uA4CF\\uF900-\\uFAFF\\uFE30-\\uFE4F\\uFF00-\\uFFEF\\u0080-\\u009F\\u2000-\\u201f\r\n]");
+    bool hasMatch = regexp.hasMatch(text);
+    if (text.isEmpty||hasMatch) {
       return text;
     }
     String breakWord = '';
@@ -159,6 +161,7 @@ class CommonUtil {
       breakWord += '\u200B';
     });
     return breakWord;
+    return text;
   }
 
   static List<String> checkHasNoMatchUids(
